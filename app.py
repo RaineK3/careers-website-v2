87688eb1c5b1b80d869a5b4c8b00713bdc74b48b
RaineK <pyae00254@gmail.com>
2023-11-17 14:05:27 +0630

    navigation according to job id

--- app.py
+++ app.py
@@ -1,51 +1,55 @@
 from flask import Flask, render_template,jsonify
-from database import engine
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id':1,
-    'title':'Data Analyst',
-    'location' : 'Tamwe,Yangon',
-    'salary' : '40Lakhs-50Lakhs(Nego)'
-  },
-  {
-    'id': 2,
-    'title':'Data Scientist',
-    'location' : 'Downtown,Yangon',
-    'salary' : '40Lakhs-50Lakhs(Nego)'
-  },
-  {
-    'id': 3,
-    'title':'Backend Engineer',
-    'location' : 'Hlaing,Yangon',
-    'salary' : '50Lakh-70Lakh(Nego)'
-  },
-  {
-    'id': 4,
-    'title':'Fronted Engineer',
-    'location' : 'Sansaung,Yangon',
-    'salary' : '50Lakh-70Lakh(Nego)'
-  },
-]
+# JOBS = [
+#   {
+#     'id':1,
+#     'title':'Data Analyst',
+#     'location' : 'Tamwe,Yangon',
+#     'salary' : '40Lakhs-50Lakhs(Nego)'
+#   },
+#   {
+#     'id': 2,
+#     'title':'Data Scientist',
+#     'location' : 'Downtown,Yangon',
+#     'salary' : '40Lakhs-50Lakhs(Nego)'
+#   },
+#   {
+#     'id': 3,
+#     'title':'Backend Engineer',
+#     'location' : 'Hlaing,Yangon',
+#     'salary' : '50Lakh-70Lakh(Nego)'
+#   },
+#   {
+#     'id': 4,
+#     'title':'Fronted Engineer',
+#     'location' : 'Sansaung,Yangon',
+#     'salary' : '50Lakh-70Lakh(Nego)'
+#   },
+# ]
 
-def load_jobs_from_db():
-  with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
 
-  result_dicts = []
-  for row in result.all():
-    row_dict = dict(row._asdict())
-    result_dicts.append(row_dict)
 
 @app.route("/")
-def hello_world():
-  return render_template("home.html",jobs = JOBS, company_name = "Jovian")
+def hello_careers():
+  JOBS = load_jobs_from_db()
+  return render_template("home.html", jobs = JOBS, company_name = "IT")
 
 @app.route('/api/jobs')
 def list_jobs():
+  JOBS = load_jobs_from_db()
   return jsonify(JOBS)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  print(job)
+  print(type(job))
+  if not job:
+    return "Not Found, 404"
+  return render_template('jobpage.html', job = job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -3,8 +3,6 @@ from sqlalchemy import *
 db_connection_string = "mysql+pymysql://root:raine@localhost/careers"
 engine = create_engine(db_connection_string)
 
-
-
   # print("type(result)", type(result))
   # result_all = result.all()
   # print("type(result.all()):", type(result_all))
@@ -15,4 +13,30 @@ engine = create_engine(db_connection_string)
   # print("type(first_result_dict):", type(first_result_dict))
   # print(first_result)
   # print(first_result_dict)
- 
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+
+    jobs = []
+    for row in result.all():
+      row_dict = dict(row._asdict())
+      jobs.append(row_dict)
+    return jobs
+  
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"select * from jobs where id = {id}"),
+    )
+
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else: 
+      return rows[0]
+      # jobs={}
+      # for row in rows:
+      #   row_dict = dict(row._asdict())
+      #   jobs.append(row_dict)
+      # return jobs
